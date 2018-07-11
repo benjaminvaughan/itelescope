173dc0230f6bf0e69cf26d498f915e1eb559512c
Benjamin James Vaughan <vaughanb18@gmail.com>
2018-07-11 23:01:45 +0000

    works up to 32 microsteps

--- stepper_motor/stepping_the_motor.py
+++ stepper_motor/stepping_the_motor.py
@@ -12,29 +12,29 @@ pi = pigpio.pi()
 pi.set_mode(DIR, pigpio.OUTPUT)
 pi.set_mode(step, pigpio.OUTPUT)
 
-pi.set_mode(switch,pio.INPUT)
+###pi.set_mode(switch,pio.INPUT)
 #pi.set_pull_up_down(switch, pigpio.PULL_DOWN)
 
 mode = (14,15,18) #microstep resolution
 resolution = {'Full':(0,0,0),
 	   'Half':(1,0,0),
 	   '1/4':(0,1,0),
-	   '1/8':(1,1,0)
+	   '1/8':(1,1,0),
 	   '1/16':(0,0,1),
-	   '1/32'(1,0,1)}
+	   '1/32':(1,0,1)}
 
 for i in range(3):
-    pi.write(MODE[i], resolution['Full'][i])
+    pi.write(mode[i], resolution['1/32'][i])
 
 pi.set_PWM_dutycycle(step,128) #PWM 1/2 on 1/2 off
-pi.set_PWM_frequency(Step,500) #500 pulses per second
+pi.set_PWM_frequency(step,1000) #500 pulses per second
 
 try:
     while True:
         # pi.write(DIR, pi.read(switch)) # set direction
         pi.write(DIR,0)
-        sleep.(1)
-except KeyBoardInterrupt:
+        sleep(1)
+except KeyboardInterrupt:
     print("\nCtrl-C presssed. Stopped pigpio and exiting...")
 finally:
     pi.set_PWM_dutycycle(step,0) #PWM off
