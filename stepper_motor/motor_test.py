8c389d96a3132c6f6f6e1522f3643f31aa6f5524
Jim <jim@mrjim.com>
2018-07-21 09:43:18 +0000

    Updated

--- stepper_motor/motor_test.py
+++ stepper_motor/motor_test.py
@@ -1,12 +1,29 @@
 
-#from motor_class import Motor
-#import pigpio
+from motor_class import Motor
+import pigpio
 
-import keyboard
+import click
 import time
 
+motor = Motor(20, 21, 14, 15, 18)
+print('Press an arrow key or q to quit')
 while True:
-    if keyboard.is_pressed('left'):
-        print('left')
-    else:
-        time.sleep(0.1)
+    key = click.getchar()
+    if key == 'q':
+        break
+    if len(key) == 3: # arrow key
+        code = ord(key[2])
+        if code == 66:
+            print('down')
+        elif code == 67:
+            print('right')
+        elif code == 68:
+            print('left')
+            motor.set_speed(128, 1000, 0)
+        elif code == 65:
+            print('up')
+        else:
+            motor.set_speed(0, 1000, 0)
+            print(code)
+
+
