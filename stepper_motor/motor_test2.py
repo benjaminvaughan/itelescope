7d977d6012d54659d7084956e99e097d22feb6b9
Benjamin James Vaughan <vaughanb18@gmail.com>
2018-08-02 23:13:34 +0000

    minor changes

--- stepper_motor/motor_test2.py
+++ stepper_motor/motor_test2.py
@@ -6,8 +6,6 @@ import click
 pi = pigpio.pi()
 motor2 = Motor(24, 23, 25, 8, 7, pi)
 motor1 = Motor(10, 9 , 17, 27, 22, pi)
-motor1.set_speed(2)
-motor2.set_speed(2)
 
 print('press an arrow key or q to quit')
 
@@ -20,12 +18,16 @@ while True:
         break
     if key == 'a':
         motor1.set_direction(1)
+        motor1.set_speed(2)
     if key == 'd':
         motor1.set_direction(0)
+        motor1.set_speed(2)
     if key == 'w':
         motor2.set_direction(1)
+        motor2.set_speed(2)
     if key == 's':
-        motor1.set_direction(0)
+        motor2.set_direction(0)
+        motor2.set_speed(2)
 
     if len(key) == 3:
         key = ord(key[2])
