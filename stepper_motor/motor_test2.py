76fc413f3e9367c5d2fdcd76c5de62209700718f
Benjamin James Vaughan <vaughanb18@gmail.com>
2018-08-02 08:22:41 +0000

    fixed some bugs

--- stepper_motor/motor_test2.py
+++ stepper_motor/motor_test2.py
@@ -3,8 +3,9 @@ import pigpio
 import time
 import click
 
-motor2 = Motor(24, 23, 25, 8, 7)
-motor1 = Motor(10, 9 , 17, 27, 22)
+pi = pigpio.pi()
+motor2 = Motor(24, 23, 25, 8, 7, pi)
+motor1 = Motor(10, 9 , 17, 27, 22, pi)
 motor1.set_speed(1)
 motor2.set_speed(2)
 
@@ -14,6 +15,8 @@ timeout = 0.1
 while True:
     key = click.getchar()
     if key == 'q':
+        motor1.stopping_motor()
+        motor2.stopping_motor()
         break
     if key == 'a':
         motor1.set_direction(1)
@@ -21,7 +24,7 @@ while True:
         motor1.set_direction(0)
     if key == 'w':
         motor2.set_direction(1)
-    if key -- 's':
+    if key == 's':
         motor1.set_direction(0)
 
     if len(key) == 3:
@@ -33,7 +36,7 @@ while True:
         if key == 65: #up
             motor1.stopping_motor()
             motor1.set_direction(1)
-            motor1.one_step
+            motor1.one_step()
         if key == 68: #right
             motor2.stopping_motor()
             motor2.set_direction(0)
@@ -41,4 +44,4 @@ while True:
         if key == 67: #left
             motor2.stopping_motor()
             motor2.set_direction(1)
-            motor2.one_step
+            motor2.one_step()
