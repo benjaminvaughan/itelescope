c36a05605ecf6a922d748058540a5c75f2fcceb9
Benjamin James Vaughan <vaughanb18@gmail.com>
2018-08-04 00:43:04 -0700

    added two modes

--- telescope_control/telescope_run.py
+++ telescope_control/telescope_run.py
@@ -1,4 +1,4 @@
-from motor_class import Motor
+gfrom motor_class import Motor
 from encoder_class import Encoder
 from telescope_class import Telescope
 import pigpio
@@ -8,30 +8,59 @@ import sys
 from calculations import Calculations
 
 if __name__ == '__main__':
-    telescope = Telescope()
-    print('enter longitude')
+    print('enter mode you would like to use')
     line = input()
-    longitude = float(line)
-    telescope.get_longitude(longitude)
-    print('enter latitude')
-    line = input()
-    latitude = float(line)
-    telescope.get_latitude(latitude)
-    print('enter right_ascension')
-    line = input()
-    degrees = float(line)
-    print('you entered %f' % degrees)
-    telescope.set_right_ascension(degrees)
-    print('enter declination')
-    line = input()
-    degrees = float(line)
-    telescope.get_gast()   
-    telescope.set_declination(degrees)
-    telescope.get_local_hour_angle()
-    telescope.set_altitude()
-    telescope.set_azimuth()
-    telescope.get_azimuth()
-    telescope.get_altitude()
-    while True:
-        telescope.update()
-        time.sleep(1)
+    if line == 'mode2':
+        print('you are now running manual mode')
+        speed = 0
+        while True:
+            key = click.getchar()
+            if len(key) == 1:
+                if key >= '1' and key <= '9':
+                    speed = int(key)
+                    print('speed %d %d' % (speed, ord(key)))
+                if key == 'q':
+                    motor1.stopping_motor()
+                    motor2.stopping_motor()
+                    break
+                if key == 'a':
+                    motor1.set_direction(1)
+                    motor1.set_speed(speed)
+                if key == 'd':
+                    motor1.set_direction(0)
+                    motor1.set_speed(speed)
+                if key == 'w':
+                    motor2.set_direction(1)
+                    motor2.set_speed(speed)
+                if key == 's':
+                    motor2.set_direction(0)
+                    motor2.set_speed(speed)
+    if line == 'mode1':
+        print('you are now running goto mode')
+        telescope = Telescope()
+        print('enter longitude')
+        line = input()
+        longitude = float(line)
+        telescope.get_longitude(longitude)
+        print('enter latitude')
+        line = input()
+        latitude = float(line)
+        telescope.get_latitude(latitude)
+        print('enter right_ascension')
+        line = input()
+        degrees = float(line)
+        print('you entered %f' % degrees)
+        telescope.set_right_ascension(degrees)
+        print('enter declination')
+        line = input()
+        degrees = float(line)
+        telescope.get_gast()   
+        telescope.set_declination(degrees)
+        telescope.get_local_hour_angle()
+        telescope.set_altitude()
+        telescope.set_azimuth()
+        telescope.get_azimuth()
+        telescope.get_altitude()
+        while True:
+            telescope.update()
+            time.sleep(1)
