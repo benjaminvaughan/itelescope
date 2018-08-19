578ab52135110630ec09bcf9adb03b2a0c0571e9
Benjamin James Vaughan <vaughanb18@gmail.com>
2018-08-19 21:08:30 +0000

    modified a little bit

--- telescope_control/debugging_telescope.py
+++ telescope_control/debugging_telescope.py
@@ -5,9 +5,12 @@ import pigpio
 import sys
 from calculations import Calculations
 import math
-import numpyy
+import numpy
 import click
 from telescope_class import Telescope
+
+
+telescope = Telescope()
 
 if __name__ == '__main__':
     pi = pigpio.pi()
@@ -15,16 +18,24 @@ if __name__ == '__main__':
     altitude_motor = Motor(24, 23, 25, 8, 7, pi)
     azimuth_encoder = Encoder(20, 21, 12, 2)
     azimuth_motor = Motor(10, 9 , 17, 27, 22)
-    azimuth = 20
-    altitude = 20
-    telescope.set_altitude(altitude)
-    telescope.set_azimuth(azimuth)
+    longitude = 20
+    latitude = 20
+    telescope.get_longitude(longitude)
+    telescope.get_latitude(latitude)
+    telescope.set_right_ascension(20)
+    telescope.set_declination(20)
+    telescope.get_gast()
+    telescope.get_local_hour_angle()
+    telescope.set_altitude()
+    telescope.set_azimuth()
     altitude_encoder.run_encoder()
     azimuth_encoder.run_altitude_encoder()
-    tele_azimuth = azimuth.encoder.get_degrees()
-    tele_altitude = altitude.encoder.get_degrees()
+    tele_azimuth = azimuth_encoder.get_degrees()
+    tele_altitude = altitude_encoder.get_degrees()
     telescope = Telescope()
+    telescope.azimuth = 200
+    telescope.altitude = 200
     while True:
-        if tele_azimuth != azimuth or tele_altitude != altitude:
+        if tele_azimuth != 20 or tele_altitude != 20:
             telescope.azimuth_update()
             telescope.altitude_update() 
